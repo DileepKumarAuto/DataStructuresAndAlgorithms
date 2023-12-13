a5c2e533c7c45461c1b602499ff2dcea8f906709
Dileep Kumar <dileepsamudrala2012@gmail.com>
2023-12-12 17:34:24 -0800

    Added comments for dp house robber

--- src/main/java/leetcode/dynamic_programming/one_dimentional/HouseRobber.java
+++ src/main/java/leetcode/dynamic_programming/one_dimentional/HouseRobber.java
@@ -2,6 +2,21 @@ package leetcode.dynamic_programming.one_dimentional;
 
 public class HouseRobber {
 
+    /**
+     * Initialize an array dp of the same length as nums to store the maximum amount that can be robbed at each house.
+     * <p>
+     * The first two elements of dp are initialized based on the maximum amount that can be robbed from the first
+     * two houses (nums[0] and nums[1]).
+     * For each subsequent house (nums[i]), calculate the maximum amount that can be robbed:
+     * The recurrence relation is defined as dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]).
+     * It means that at each house, you compare the maximum amount that can be robbed if you rob the
+     * current house (dp[i - 2] + nums[i]) with the maximum amount that can be robbed without robbing the
+     * current house (dp[i - 1]).
+     * The maximum of these two values is then stored in dp[i].
+     * <p>
+     * The final result is the maximum amount that can be robbed from the last house,
+     * so the function returns dp[nums.length - 1].
+     */
     int rob(int[] nums) {
 
         if (nums.length < 2)
@@ -19,4 +34,9 @@ public class HouseRobber {
 
         return dp[nums.length - 1];
     }
+
+    /**
+     * Time Complexity: O(n) where n is the length of the array
+     * Space Complexity: O(n) where n is the length of the array
+     */
 }
