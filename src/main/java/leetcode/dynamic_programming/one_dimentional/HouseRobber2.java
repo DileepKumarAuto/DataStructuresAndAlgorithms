efde107362efae9bd5226baa4eb795ff919b8a24
Dileep Kumar <dileepsamudrala2012@gmail.com>
2023-12-15 16:51:15 -0800

    Added comments for House robber 2 problem

--- src/main/java/leetcode/dynamic_programming/one_dimentional/HouseRobber2.java
+++ src/main/java/leetcode/dynamic_programming/one_dimentional/HouseRobber2.java
@@ -2,6 +2,30 @@ package leetcode.dynamic_programming.one_dimentional;
 
 public class HouseRobber2 {
 
+    /**
+     * If there are fewer than two houses, return the only available value as it is the maximum possible loot.
+     *
+     * Create two arrays, skipLastHouse and skipFirstHouse, each excluding one of the adjacent houses in the
+     * circular arrangement.
+     * skipLastHouse excludes the last house, and skipFirstHouse excludes the first house.
+     *
+     * Call the robHelper function for both arrays to calculate the maximum loot for the two possibilities:
+     * lootSkippingLast: Maximum loot when excluding the last house.
+     * lootSkippingFirst: Maximum loot when excluding the first house.
+     *
+     * Return the maximum of the two calculated loots (lootSkippingLast and lootSkippingFirst).
+     *
+     * The robHelper function is a standard dynamic programming approach to solving the House Robber problem
+     * for a linear arrangement of houses.
+     * It calculates the maximum loot by considering the options of robbing or skipping each house.
+     *
+     * Use dynamic programming to fill the dp array, where dp[i] represents the maximum loot that can be
+     * obtained up to house i.
+     * The recurrence relation is dp[i] = max(dp[i-2] + nums[i], dp[i-1]).
+     * Return Maximum Loot from dp:
+     *
+     * Return the last element of the dp array, which represents the maximum loot for the linear arrangement of houses.
+     */
     int rob(int[] nums) {
 
         if (nums.length < 2)
@@ -42,8 +66,8 @@ public class HouseRobber2 {
     }
 
     /**
-     * Time complexity : O(N)
-     * Space complexity : O(N)
+     * Time complexity: O(n) - Linear, where 'n' is the number of houses.
+     * Space complexity: O(n) - Linear, where 'n' is the number of houses.
      */
 
 
