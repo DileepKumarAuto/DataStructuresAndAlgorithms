e0545122df109fe18d6684b0e5236b6b05b8d694
Dileep Kumar <dileepsamudrala2012@gmail.com>
2023-12-16 20:50:27 -0800

    Added valid anagrams

---
+++ src/main/java/leetcode/arrays/ValidAnagrams.java
@@ -0,0 +1,18 @@
+package leetcode.arrays;
+
+public class ValidAnagrams {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] store = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            store[s.charAt(i) - 'a']++;
+            store[t.charAt(i) - 'a']--;
+        }
+
+        for (int n : store) if (n != 0) return false;
+
+        return true;
+    }
+}
