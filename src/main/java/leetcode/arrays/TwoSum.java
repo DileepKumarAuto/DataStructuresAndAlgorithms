5454be4e637b25cd3f30553914a60f839eac27a1
Dileep Kumar <dileepsamudrala2012@gmail.com>
2023-12-16 20:40:41 -0800

    Added two sum program

---
+++ src/main/java/leetcode/arrays/TwoSum.java
@@ -0,0 +1,23 @@
+package leetcode.arrays;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> prevMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (prevMap.containsKey(diff)) {
+                return new int[] { prevMap.get(diff), i };
+            }
+
+            prevMap.put(num, i);
+        }
+
+        return new int[] {};
+    }
+}
