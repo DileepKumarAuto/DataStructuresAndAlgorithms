e780636a3b4b8ae41759fe1c085368de99a73ff5
Dileep Kumar <dileepsamudrala2012@gmail.com>
2023-12-19 05:51:46 -0800

    Added maximum product sub array

---
+++ src/main/java/leetcode/dynamic_programming/one_dimentional/MaximumProductSubArray.java
@@ -0,0 +1,29 @@
+package leetcode.dynamic_programming.one_dimentional;
+
+public class MaximumProductSubArray {
+
+    int maxProduct(int[] nums) {
+
+        int n = nums.length;
+        int leftProduct = 1;
+        int rightProduct = 1;
+        int ans = nums[0];
+
+        for (int i = 0; i < n; i++) {
+
+            //if any of leftProduct or rightProduct become 0 then update it to 1
+            leftProduct = leftProduct == 0 ? 1 : leftProduct;
+            rightProduct = rightProduct == 0 ? 1 : rightProduct;
+
+            //prefix product
+            leftProduct *= nums[i];
+
+            //suffix product
+            rightProduct *= nums[n - 1 - i];
+
+            ans = Math.max(ans, Math.max(leftProduct, rightProduct));
+        }
+
+        return ans;
+    }
+}
