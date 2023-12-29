d8060990dc87010bbc347eebee63fbb73848c98b
dileep <dileepsamudrala2012@gmail.com>
2023-12-28 19:06:57 -0800

    Added container with most water

---
+++ src/main/java/leetcode/two_pointer/ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+package leetcode.two_pointer;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int res = 0;
+        while (left < right) {
+            int containerLength = right - left;
+            int area = containerLength * Math.min(height[left], height[right]);
+            res = Math.max(res, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return res;
+    }
+}
