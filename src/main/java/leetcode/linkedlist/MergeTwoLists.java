6164b9ab8a95d286079c3f4d8ee7fcc0b60c6c67
dileep <dileepsamudrala2012@gmail.com>
2024-01-03 19:13:19 -0800

    Added comments and T&S complexity for merge two sorted linked lists

--- src/main/java/leetcode/linkedlist/MergeTwoLists.java
+++ src/main/java/leetcode/linkedlist/MergeTwoLists.java
@@ -2,6 +2,19 @@ package leetcode.linkedlist;
 
 public class MergeTwoLists {
 
+    /**
+     * Create a new ListNode called root to serve as the dummy node for the merged list.
+     * Initialize a pointer prev to keep track of the last node in the merged list.
+     * Use a while loop to iterate through both lists until either one becomes null.
+     * Inside the loop:
+     * Compare the current nodes from both lists (list1 and list2).
+     * Connect the smaller node to the merged list through the prev pointer.
+     * Move the pointer of the list from which the node was taken to its next node.
+     * Move the prev pointer to the newly added node in the merged list.
+     * After the loop, connect the remaining nodes of the non-empty list (if any) to the merged list.
+     * Return the next node of the root, which is the head of the merged sorted list.
+     */
+
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         final ListNode root = new ListNode();
         ListNode prev = root;
@@ -18,4 +31,9 @@ public class MergeTwoLists {
         prev.next = list1 != null ? list1 : list2;
         return root.next;
     }
+
+    /**
+     * Time Complexity: O(m + n), where m and n are the lengths of the input lists.
+     * Space Complexity: O(1) - constant space is used regardless of the input size.
+     * */
 }
