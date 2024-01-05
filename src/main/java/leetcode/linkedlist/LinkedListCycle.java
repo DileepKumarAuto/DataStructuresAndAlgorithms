777ef2c48e3520700c26899ce0dfd90663c9d095
dileep <dileepsamudrala2012@gmail.com>
2024-01-04 18:48:30 -0800

    Added Linked List cycle detection problem

---
+++ src/main/java/leetcode/linkedlist/LinkedListCycle.java
@@ -0,0 +1,14 @@
+package leetcode.linkedlist;
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
