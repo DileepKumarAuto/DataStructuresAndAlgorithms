a6b7709c1222ba9e8f021476f803188f75e8b0e6
dileep <dileepsamudrala2012@gmail.com>
2024-01-05 17:24:10 -0800

    Added comments and T&S complexity for remove nth node from end

--- src/main/java/leetcode/linkedlist/RemoveNthNodeFromEnd.java
+++ src/main/java/leetcode/linkedlist/RemoveNthNodeFromEnd.java
@@ -1,6 +1,16 @@
 package leetcode.linkedlist;
 
 public class RemoveNthNodeFromEnd {
+
+    /**
+     * Create a dummy node 'temp' and set its next to the head of the linked list to handle edge cases.
+     * Initialize two pointers, 'first' and 'second,' both pointing to the dummy node.
+     * Move the 'second' pointer n nodes ahead.
+     * Move both 'first' and 'second' pointers until 'second' reaches the end of the linked list.
+     * 'first' will be pointing to the node before the one to be removed.
+     * Adjust the next reference of 'first' to skip the node to be removed.
+     * Return the modified linked list starting from temp.next.
+     *  */
     public ListNode removeNthFromEnd(ListNode head, int n) {
         if (head == null || head.next == null) return null;
 
@@ -21,4 +31,13 @@ public class RemoveNthNodeFromEnd {
         first.next = first.next.next;
         return temp.next;
     }
+
+    /**
+     * Time Complexity: O(n) The algorithm traverses the linked list twice,
+     * with the 'second' pointer moving n steps and then reaching the end.
+     *
+     * Space Complexity: O(1) Constant space is used regardless of the input size, as only
+     * two pointers ('first' and 'second') and a dummy node are utilized.
+     */
+
 }
