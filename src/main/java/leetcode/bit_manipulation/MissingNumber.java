61201cbb0ad0af5404566c72239df97430250aaa
dileep <dileepsamudrala2012@gmail.com>
2024-01-06 12:47:47 -0800

    Added missing number

---
+++ src/main/java/leetcode/bit_manipulation/MissingNumber.java
@@ -0,0 +1,13 @@
+package leetcode.bit_manipulation;
+
+public class MissingNumber {
+
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        int total = nums.length * (nums.length + 1) / 2;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return total - sum;
+    }
+}
