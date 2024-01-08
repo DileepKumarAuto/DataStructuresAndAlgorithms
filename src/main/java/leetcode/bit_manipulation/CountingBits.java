bb64492e0312cf2458c35e84b3882c4d614853da
dileep <dileepsamudrala2012@gmail.com>
2024-01-07 21:13:33 -0800

    Added comments and T&S complexity for counting bits

--- src/main/java/leetcode/bit_manipulation/CountingBits.java
+++ src/main/java/leetcode/bit_manipulation/CountingBits.java
@@ -2,6 +2,14 @@ package leetcode.bit_manipulation;
 
 public class CountingBits {
 
+    /**
+     * Use a bitmask (mask) with a 1 in the least significant bit.
+     * Iterate through each bit (32 bits in a 32-bit integer).
+     * Check if the bit at the current position is 1 by using bitwise AND operation.
+     * If the result is not 0, increment the count.
+     * Right-shift the original number to check the next bit.
+     * */
+
     public int hammingWeight(int n) {
         int count = 0;
         int mask = 1;
@@ -13,4 +21,9 @@ public class CountingBits {
         }
         return count;
     }
+    /**
+     *
+     * Time Complexity: O(1) : The algorithm iterates through each bit in a fixed-size 32-bit integer.
+     * Space Complexity: O(1) : Constant space is used regardless of the input size.
+     */
 }
