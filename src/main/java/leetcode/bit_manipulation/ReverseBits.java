479474d36be6078b6c9994dd9a6598539212f994
dileep <dileepsamudrala2012@gmail.com>
2024-01-09 19:46:49 -0800

    Added Reverse bits

---
+++ src/main/java/leetcode/bit_manipulation/ReverseBits.java
@@ -0,0 +1,15 @@
+package leetcode.bit_manipulation;
+
+public class ReverseBits {
+
+    public int reverseBits(int n) {
+        int ans = 0;
+
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
+        }
+        return ans;
+    }
+}
