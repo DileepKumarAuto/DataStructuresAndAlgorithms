21ed6898f84b4194a4724dd4d3ab75db9223840a
dileep <dileepsamudrala2012@gmail.com>
2024-01-13 17:54:31 -0800

    Added comments and time and space complexity invert tree program

--- src/main/java/leetcode/trees/InvertTree.java
+++ src/main/java/leetcode/trees/InvertTree.java
@@ -2,7 +2,16 @@ package leetcode.trees;
 
 public class InvertTree {
 
-
+    /**
+     * Base Case:  If the root of the tree is null, return null, as there is no subtree to invert.
+     *
+     * Create a new TreeNode named node with the same value as the original root.
+     * Invert Left and Right Subtrees:
+     * Recursively invert the left subtree of the original root and assign it to the right child of the new node.
+     * Recursively invert the right subtree of the original root and assign it to the left child of the new node.
+     *
+     * Return the node as the root of the inverted subtree.
+     */
 
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
@@ -12,5 +21,9 @@ public class InvertTree {
         return node;
     }
 
-
+    /**
+     * Time complexity:  O(n), where n is the number of nodes in the tree, as each node is visited once.
+     * Space complexity is O(h), where h is the height of the tree.
+     * This represents the maximum height of the recursive call stack.
+     */
 }
