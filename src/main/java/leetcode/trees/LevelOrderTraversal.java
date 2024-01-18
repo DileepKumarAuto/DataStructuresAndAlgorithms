0165d2f31049440d4fd65eac391660f4c0efc8d3
dileep <dileepsamudrala2012@gmail.com>
2024-01-18 15:06:59 -0800

    Added comments for Level order traversal

--- src/main/java/leetcode/trees/LevelOrderTraversal.java
+++ src/main/java/leetcode/trees/LevelOrderTraversal.java
@@ -7,6 +7,31 @@ import java.util.Queue;
 
 public class LevelOrderTraversal {
 
+    /**
+     * Initialization:
+     * Initialize an empty list of lists res to store the result.
+     * Create a queue (LinkedList) to facilitate the breadth-first traversal.
+     *
+     * Breadth-First Traversal:     *
+     * Enqueue the root node if it exists.
+     * While the queue is not empty:
+     *      Get the current level's size (len) to iterate through the nodes at the current level.
+     *      Create a list (level) to store the values of nodes at the current level.
+     *      Iterate len times:
+     *          Dequeue a node (curr) from the queue.
+     *          Add the value of curr to the level list.
+     *          Enqueue the left and right children of curr if they exist.
+     *
+     * Add Level List to Result:
+     * After processing all nodes at the current level, add the level list to the result res.
+     *
+     * Repeat Until Queue is Empty:
+     * Continue the process until the queue is empty, ensuring that all levels are processed.
+     *
+     *  Return Result:
+     *  Return the final result res, containing lists of nodes at each level.
+     */
+
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
@@ -31,4 +56,11 @@ public class LevelOrderTraversal {
         }
         return res;
     }
+
+    /**
+     * Time Complexity: O(n), where n is the number of nodes in the binary tree. Each node is processed once.
+     * Space Complexity: O(m), where m is the maximum number of nodes at any level.
+     * In the worst case, the maximum number of nodes at a level is n/2 (for a perfectly balanced tree).
+     * The space complexity is dominated by the queue.
+     */
 }
