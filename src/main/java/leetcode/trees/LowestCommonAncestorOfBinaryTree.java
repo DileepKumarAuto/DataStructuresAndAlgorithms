a7a88ecaa55626346431c734939523bbe1c78a0f
dileep <dileepsamudrala2012@gmail.com>
2024-01-17 20:06:49 -0800

    Added lowest common ancestor of binary tree

---
+++ src/main/java/leetcode/trees/LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,22 @@
+package leetcode.trees;
+
+public class LowestCommonAncestorOfBinaryTree {
+
+    public TreeNode lowestCommonAncestor(
+            TreeNode root,
+            TreeNode p,
+            TreeNode q
+    ) {
+        if (p.val > root.val && q.val > root.val) return lowestCommonAncestor(
+                root.right,
+                p,
+                q
+        );
+        if (p.val < root.val && q.val < root.val) return lowestCommonAncestor(
+                root.left,
+                p,
+                q
+        );
+        return root;
+    }
+}
