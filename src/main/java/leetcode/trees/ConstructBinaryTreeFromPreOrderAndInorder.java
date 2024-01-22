f733ce6666afb70362bb098a49e42ce268a04161
dileep <dileepsamudrala2012@gmail.com>
2024-01-21 18:20:46 -0800

    Added comments for build tree

--- src/main/java/leetcode/trees/ConstructBinaryTreeFromPreOrderAndInorder.java
+++ src/main/java/leetcode/trees/ConstructBinaryTreeFromPreOrderAndInorder.java
@@ -4,6 +4,31 @@ import java.util.Arrays;
 
 public class ConstructBinaryTreeFromPreOrderAndInorder {
 
+    /**
+     * Base Case Check:
+     * If either the preorder array or the inorder array is empty, return null since there are no elements to
+     * construct the tree.
+     * <p>
+     * Create Root Node:
+     * Create a TreeNode named root with the value of the first element from the preorder array (preorder[0]).
+     * <p>
+     * Find Root Position in Inorder:
+     * Iterate through the inorder array to find the index (mid) where the value of the root node matches (preorder[0]).
+     * <p>
+     * Construct Left Subtree:
+     * Recursively call buildTree with the left subtree's preorder and inorder arrays.
+     * preorder for the left subtree is obtained by copying elements from index 1 to mid + 1.
+     * inorder for the left subtree is obtained by copying elements from index 0 to mid.
+     * <p>
+     * Construct Right Subtree:
+     * Recursively call buildTree with the right subtree's preorder and inorder arrays.
+     * preorder for the right subtree is obtained by copying elements from index mid + 1 to the end.
+     * inorder for the right subtree is obtained by copying elements from index mid + 1 to the end.
+     * <p>
+     * Return Root Node:
+     * Return the constructed root node.
+     */
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         if (preorder.length == 0 || inorder.length == 0) return null;
 
@@ -26,4 +51,11 @@ public class ConstructBinaryTreeFromPreOrderAndInorder {
 
         return root;
     }
+
+    /**
+     *Time Complexity: O(n^2) - In the worst case, each recursive call involves copying arrays,
+     * leading to quadratic time complexity.
+     * Space Complexity: O(n) - The space complexity is dominated by the recursive call stack,
+     * which can go as deep as the number of nodes in the tree.
+     */
 }
