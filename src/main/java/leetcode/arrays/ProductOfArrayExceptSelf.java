975579be5a57625298a54ebd917853946f07b686
dileep <dileepsamudrala2012@gmail.com>
2024-01-22 17:35:06 -0800

    Added product of array except self array

---
+++ src/main/java/leetcode/arrays/ProductOfArrayExceptSelf.java
@@ -0,0 +1,19 @@
+package leetcode.arrays;
+
+public class ProductOfArrayExceptSelf {
+
+    public int[] productExceptSelf(int[] nums) {
+        int[] arr = new int[nums.length];
+        int right = 1, left = 1;
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = left;
+            left *= nums[i];
+        }
+        for (int i = nums.length - 1; i >= 0; i--) {
+            arr[i] *= right;
+            right *= nums[i];
+        }
+        return arr;
+    }
+
+}
