9daa6cafdc999d565ca823369985fb2b48975c1d
dileep <dileepsamudrala2012@gmail.com>
2024-01-27 22:04:41 +0530

    Added comments for insertion sort

--- src/main/java/code/sorting/InsertionSort.java
+++ src/main/java/code/sorting/InsertionSort.java
@@ -2,6 +2,16 @@ package code.sorting;
 
 public class InsertionSort {
 
+    /**
+     * Initialization:
+     * Start with the second element of the array (index 1) as the first element is considered already sorted.
+     * Iterate through the unsorted part of the array, comparing each element with the elements in the sorted sublist.
+     *
+     * Insertion:
+     * For each element in the unsorted part, compare it with the elements in the sorted sublist from right to left.
+     * Shift larger elements one position to the right until finding the correct position for insertion.
+     * Insert the current element into its correct position in the sorted sublist.
+     */
 
     public static void insertionSort(int[] arr) {
         int n = arr.length;
@@ -17,4 +27,9 @@ public class InsertionSort {
         }
     }
 
+    /**
+     * Time complexity : O(n^2)  where n is the number of elements in the array.
+     * Space Complexity: O(1) because insertion sort only requires a constant amount of additional memory space for
+     * variables like indices and temporary variables.
+     */
 }
