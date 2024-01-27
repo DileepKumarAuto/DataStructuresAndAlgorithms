cab3f46c251d00473046ac50afaa565fc3c3aec4
dileep <dileepsamudrala2012@gmail.com>
2024-01-27 05:46:54 +0530

    Added comments for selection sort

--- src/main/java/code/sorting/SelectionSort.java
+++ src/main/java/code/sorting/SelectionSort.java
@@ -2,7 +2,26 @@ package code.sorting;
 
 public class SelectionSort {
 
-
+    /**
+     * Initialization:
+     * Start with the entire array as the unsorted part.
+     * Initialize an outer loop to iterate over the unsorted part from the first element to the second-to-last element.
+     *
+     * Find Minimum Element:
+     * Inside the outer loop, initialize a variable minIndex to the current index of the outer loop (i).
+     * Start an inner loop from i+1 to the end of the array.
+     * In the inner loop, compare each element with the element at minIndex. If the current element is smaller,
+     * update minIndex with the current index.
+     *
+     * Swap Minimum Element:
+     * After finding the minimum element in the unsorted part, swap it with the first element in the unsorted part.
+     * The first element in the unsorted part is at index i.
+     *
+     * Repeat:
+     * Move the boundary between the sorted and unsorted parts one element to the right by incrementing the outer
+     * loop index (i).
+     * Repeat steps 2 and 3 until the entire array is sorted.
+     */
 
    public static void selectionSort(int[] arr) {
         int n = arr.length;
@@ -20,5 +39,10 @@ public class SelectionSort {
         }
     }
 
-
+    /**
+     * Time Complexity: O(n^2) - Selection sort has a quadratic time complexity, making it inefficient
+     * for large datasets.
+     * Space Complexity: O(1) - Selection sort operates in-place and requires a constant amount of additional m
+     * emory for temporary variables, making its space complexity constant.
+     */
 }
